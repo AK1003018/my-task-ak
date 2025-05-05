b18256c4c62ab578c9b5c31af13b86eaec002213
Anish K <65635876+AK1003018@users.noreply.github.com>
2025-05-06 00:09:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const subtaskRoutes = require('./routes/subtasks');
 const app = express();
 const port = 3000;
 
-app.use(cors());
+app.use(cors({origin: 'https://my-tasks-new.vercel.app/'}));
 app.use(express.json());
 
 app.use('/api', authRoutes);
